9364359caa35ac11b4ddaab72e2558b929978533
PauSabater <pau.sabater.vilar@gmail.com>
2024-07-11 20:25:16 +0200

    test server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react"
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
-    output: 'hybrid',
+    output: 'server',
     adapter: vercel()
 })
